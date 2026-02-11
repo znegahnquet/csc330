5c28c2f2a2fbf585ebae190f89249ad09de573dd
znegahnquet <znegahnquet10@gmail.com>
2026-02-11 00:22:51 +0000

    Add ZOMBIE

--- ZOMBIE.py
+++ ZOMBIE.py
@@ -4,10 +4,9 @@ import time
 pid = os.fork()
 
 if pid == 0:  # Child process
-    print(f"Child ({os.getpid()}): Working for 30 seconds...")
-    time.sleep(30)
+    print(f"Child ({os.getpid()}) started")
     print(f"Child ({os.getpid()}) finished")
 else:  # Parent process
-    print(f"Parent ({os.getpid()}) started")
+    print(f"Parent ({os.getpid()}): Working for 30 seconds...")
+    time.sleep(30)
     print(f"Parent ({os.getpid()}) finished")
-
